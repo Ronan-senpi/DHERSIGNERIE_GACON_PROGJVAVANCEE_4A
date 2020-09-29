c0cf12e5851f05a0c89249ddac2f100f3d8149e7
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2020-09-29 13:29:52 +0200

    Flash object, Mine oject

--- Assets/Scripts/BombeBaseScript.cs
+++ Assets/Scripts/BombeBaseScript.cs
@@ -7,7 +7,7 @@ public class BombeBaseScript : MonoBehaviour
     [SerializeField]
     protected float timer = 1.5f;
     [SerializeField]
-    protected GameObject FireObject;
+    protected GameObject explosionObject;
     [SerializeField]
     protected int range = 1;
     // Start is called before the first frame update
@@ -23,9 +23,9 @@ public class BombeBaseScript : MonoBehaviour
 
     }
 
-    protected virtual void Explosion()
+    public virtual void Explosion()
     {
-        Instantiate(FireObject, gameObject.transform.position, Quaternion.identity);
+        Instantiate(explosionObject, gameObject.transform.position, Quaternion.identity);
         for (int i = 1; i <= range; i++)
         {
             //Pas opti mais pour le moment ... 
@@ -33,26 +33,26 @@ public class BombeBaseScript : MonoBehaviour
                 gameObject.transform.position.x + i,
                 gameObject.transform.position.y,
                 gameObject.transform.position.z);
-            var tmp1 = Instantiate(FireObject, nPosition1, Quaternion.identity);
+            var tmp1 = Instantiate(explosionObject, nPosition1, Quaternion.identity);
             tmp1.name = "1";
 
             Vector3 nPosition2 = new Vector3(
                 gameObject.transform.position.x - i,
                 gameObject.transform.position.y,
                 gameObject.transform.position.z);
-            var tmp2 = Instantiate(FireObject, nPosition2, Quaternion.identity);
+            var tmp2 = Instantiate(explosionObject, nPosition2, Quaternion.identity);
             tmp2.name = "2";
             Vector3 nPosition3 = new Vector3(
                 gameObject.transform.position.x,
                 gameObject.transform.position.y,
                 gameObject.transform.position.z + i);
-            var tmp3 = Instantiate(FireObject, nPosition3, Quaternion.identity);
+            var tmp3 = Instantiate(explosionObject, nPosition3, Quaternion.identity);
             tmp3.name = "3";
             Vector3 nPosition4 = new Vector3(
                 gameObject.transform.position.x,
                 gameObject.transform.position.y,
                 gameObject.transform.position.z - i);
-            var tmp4 = Instantiate(FireObject, nPosition4, Quaternion.identity);
+            var tmp4 = Instantiate(explosionObject, nPosition4, Quaternion.identity);
             tmp4.name = "4";
 
         }
--- Assets/Scripts/ExplosionScript.cs
+++ Assets/Scripts/ExplosionScript.cs
@@ -2,28 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExplosionScript : MonoBehaviour
+public class ExplosionScript : ExplosionBaseScript
 {
     [SerializeField]
-    protected LayerMask DestroableLayer;
-    [SerializeField]
-    protected float Duration = 1f;
-    protected virtual void Start()
+    protected LayerMask BombLayer;
+
+    protected override void OnTriggerEnter(Collider other)
     {
-        StartCoroutine(EndExplosionAfterDuration());   
-    }
-    protected IEnumerator EndExplosionAfterDuration()
-    {
-        yield return new WaitForSeconds(Duration);
-        Destroy(gameObject);
-    }
-    protected virtual void OnTriggerEnter(Collider other)
-    {
-        //Compare sur les bit et non sur la text plus rapide
-        if ((DestroableLayer.value & (1 << other.gameObject.layer)) > 0)
+        base.OnTriggerEnter(other);
+        if ((BombLayer.value & (1 << other.gameObject.layer)) > 0)
         {
-            Debug.Log("You blow something you can blow !! :o ");
-            Destroy(other.gameObject);
+            BombeBaseScript bbs = other.GetComponent<BombeBaseScript>();
+            if (bbs != null)
+            {
+                bbs.Explosion();
+                Destroy(other.gameObject);
+
+            }
         }
     }
 }
