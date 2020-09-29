667b6318c27769d6867f82ed6f79efd622c3c712
YumiD <gacon.dorian@gmail.com>
2020-09-29 13:19:39 +0200

    Keyboard movements + borders + quelques walls

---
+++ Assets/Scripts/PlayerDeplacementScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeplacementScript : MonoBehaviour
+{
+
+    [SerializeField] private float Speed = 3.0f;
+    [SerializeField] private string AxisHorizontal;
+    [SerializeField] private string AxisVertical;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 direction = Vector3.zero;
+
+
+        if (Input.GetAxis(AxisHorizontal) > 0 ){
+            direction.z++;
+        }
+
+        if (Input.GetAxis(AxisHorizontal) < 0 ){
+            direction.z--;
+        }
+
+        if (Input.GetAxis(AxisVertical) > 0 ){
+            direction.x--;
+        }
+
+        if (Input.GetAxis(AxisVertical) < 0 ){
+            direction.x++;
+        }
+
+
+        gameObject.transform.position += direction * Time.deltaTime * Speed;
+        
+    }
+}
