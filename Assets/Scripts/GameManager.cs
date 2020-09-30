e20f8f5836af4ffd594d38f9f3775b009b77aa3b
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2020-09-30 09:53:32 +0200

    TMP - Score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject PlayerPrefab;
+    private readonly string P1_SCORE_TXT = "Player1 : ";
+    private readonly string P2_SCORE_TXT = "Player2 : ";
+    private readonly string WINNER = " win !";
+    [SerializeField] 
+    private GameObject PlayerPrefab;
+    [SerializeField]
+    private int maxScore = 3;
+    [SerializeField]
+    private TextMeshProUGUI p1ScoreTxt;
+    private int p1Score = 0;
+    [SerializeField]
+    private TextMeshProUGUI p2ScoreTxt;
+    private int p2Score = 0;
+
+    [SerializeField]
+    private TextMeshProUGUI winnerTxt;
     // Start is called before the first frame update
     void Start()
     {
         StartGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndRound(int playerId)
     {
+        if (playerId == 1)
+        {
+            p2Score++;
+            p2ScoreTxt.text = P2_SCORE_TXT + p2Score.ToString();
+        }
+        else
+        {
+            p1Score++;
+            p1ScoreTxt.text = P1_SCORE_TXT + p1Score.ToString();
+            if (true)
+            {
+
+            }
+        }
 
     }
-
-    public void StartGame(){
+    public void StartGame()
+    {
+        p1ScoreTxt.text = P1_SCORE_TXT + p1Score.ToString();
+        p2ScoreTxt.text = P2_SCORE_TXT + p2Score.ToString();
+        winnerTxt.enabled = false;
         //Instantiate<GameObject>(PlayerPrefab, Vector3.zero, Quaternion.identity);
         //Instantiate<GameObject>(PlayerPrefab, Vector3.one, Quaternion.identity);
     }
 
-    public void RestartGame(){
+    public void RestartGame()
+    {
         //Instantiate<GameObject>(PlayerPrefab, Vector3.zero, Quaternion.identity);
-        Application.LoadLevel(Application.loadedLevel);
+        //Application.LoadLevel(Application.loadedLevel);
     }
 }
