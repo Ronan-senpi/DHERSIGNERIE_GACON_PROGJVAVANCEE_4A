b7ec5860becfa3f580dde6df209a660568b0b53f
YumiD <gacon.dorian@gmail.com>
2020-10-01 12:33:14 +0200

    Bomb choice in the menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,6 +35,10 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         StartGame();
+        
+        Debug.Log("P1 : " + PlayerPrefs.GetString("BombP1", "No"));
+        
+        Debug.Log("P2 : " + PlayerPrefs.GetString("BombP2", "No"));
     }
 
     public void EndRound(int playerId)
---
+++ Assets/Scripts/others/BombDropdownHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BombDropdownHandler : MonoBehaviour
+{
+
+
+    private void Start() {
+        HandleInputDataP1(GetComponent<Dropdown>().value);
+        HandleInputDataP2(GetComponent<Dropdown>().value);
+    }
+    public void HandleInputDataP1(int val)
+    {
+        switch(val){
+            case 0:
+                PlayerPrefs.SetString("BombP1","Flash");
+            break;
+            case 1:
+                PlayerPrefs.SetString("BombP1","Mine");
+            break;
+            default:
+            break;
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void HandleInputDataP2(int val)
+    {
+        switch(val){
+            case 0:
+                PlayerPrefs.SetString("BombP2","Flash");
+            break;
+            case 1:
+                PlayerPrefs.SetString("BombP2","Mine");
+            break;
+            default:
+            break;
+        }
+        PlayerPrefs.Save();
+    }
+}
